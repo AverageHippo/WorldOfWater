043c279d5a0bcc1e9fdb10bc1e22fd69270f1bfe
Alaric Besharah <acb656@mail.usask.ca>
2019-07-11 10:42:11 -0600

    smal update

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+
+// Array of items to be displayed on the site, 
+// ORDER IS VERY IMPORTANT: name, description, path to image file
+// eg. ["Water cooler", "An awesome water cooler", ""] 
+const items = [
+    ["name", "description", "imageName"],
+    ["","",""],
+    ["","",""]
+];
+
+/**
+ * 
+ */
 function createProducts(){
     alert("linked correctly");
 }
